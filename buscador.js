bb13221d8b98b24f842ed49681cefddedfae0173
CarlosSintesis30 <peraltacarlos30@gmail.com>
2023-05-31 08:31:42 -0600

    actulizacion de js y formulario

--- buscador.js
+++ buscador.js
@@ -1,32 +1,153 @@
-/*******buscador */
+
+/*******buscador *******************************************/
 const searchInput = document.getElementById('search-input');
 const searchButton = document.getElementById('search-button');
-const itemContainer = document.getElementById('item-container');
+const searchResults = document.getElementById('search-results');
+const linea=document.querySelector('.linea');
+const caja1 = document.querySelector('.caja1');
+const titulo_container_item=document.querySelectorAll('.titulo-container-item');
 
+// Obtener todos los elementos con la clase 'item' 
+const items = Array.from(document.querySelectorAll('.container-items .item')).map(item => {
+  return {
+    name: item.querySelector('h3').textContent.trim().toLowerCase(),
+    element: item
+  };
+});
+
+searchInput.addEventListener('input', searchItems);
 searchButton.addEventListener('click', searchItems);
-searchInput.addEventListener('keyup', e => {
-  if (e.key === 'Enter') {
+searchInput.addEventListener('keyup', (event) => {
+  if (event.key === 'Enter') {
     searchItems();
+  }
+});
+
+// Ocultar la lista de resultados al hacer clic fuera de ella
+document.addEventListener('click', (event) => {
+  if (!searchResults.contains(event.target) && event.target !== searchInput) {
+    searchResults.style.display = 'none';
   }
 });
 
 function searchItems() {
   const searchTerm = searchInput.value.toLowerCase();
-  const items = Array.from(itemContainer.getElementsByClassName('item'));
+  const matchingItems = items.filter(item => item.name.includes(searchTerm));
 
-  items.forEach(item => {
-    const itemName = item.querySelector('h3').textContent.toLowerCase();
+  if(searchTerm===''){
+    hideSelectedItem(); // Ocultar el elemento seleccionado
+  }
+  displayResults(matchingItems);
+}
 
-    if (itemName.includes(searchTerm)) {
-      item.classList.remove('hidden1');
-    } else {
-      item.classList.add('hidden1');
+function displayResults(matchingItems) {
+  searchResults.innerHTML = '';
+  const uniqueItems = [];
+
+  if (matchingItems.length > 0) {
+    matchingItems.forEach(item => {
+      if (!uniqueItems.includes(item.name)) {
+        uniqueItems.push(item.name);
+
+        const listItem = document.createElement('li');
+        listItem.textContent = item.name;
+        listItem.addEventListener('click', () => {
+          searchInput.value = item.name;
+          showSelectedItem(item);
+        });
+        searchResults.appendChild(listItem);
+      }
+    });
+
+    // Agregar animación al mostrar el resultado
+    searchResults.classList.add('animate__animated', 'animate__fadeInDown');
+    searchResults.style.display = 'block';
+  } else {
+    const listItem = document.createElement('li');
+    listItem.textContent = 'No se encontraron resultados.';
+    searchResults.appendChild(listItem);
+    searchResults.style.display = 'block';
+  }
+
+  if (searchInput.value === '') {
+    searchResults.style.display = 'none'; // Oculta la lista de resultados
+    showAllItems();
+  }
+}
+
+function showSelectedItem(item) {
+  const containerItems = document.querySelectorAll('.container-items .item');
+
+  containerItems.forEach(item => {
+    item.classList.add('ocultarItem');
+  });
+
+  item.element.classList.remove('ocultarItem');
+  item.element.classList.remove('ocultar');
+
+  searchResults.innerHTML = '';
+  searchResults.style.display = 'none';
+
+  // Agrega la clase de animación al elemento
+  item.element.classList.add('animate__animated', 'animate__fadeIn', 'selected-item');
+  linea.style.display='none', //oculta la linea divisora
+  caja1.style.display = 'none'; //oculta el slider y las imágenes
+  titulo_container_item.forEach(titulo_container_item => { // oculta los títulos de los contenedores de items
+    titulo_container_item.style.display = 'none';
+  });
+}
+
+function showAllItems() {
+  const containerItems = document.querySelectorAll('.container-items .item');
+
+  containerItems.forEach(item => {
+    item.classList.remove('ocultarItem');
+  });
+  
+  // Elimina la clase 'selected-item' de todos los elementos
+  containerItems.forEach(item => {
+    item.classList.remove('selected-item');
+  });
+
+  // Agregar animación al mostrar los elementos
+  containerItems.forEach(item => {
+    item.classList.add('animate__animated', 'animate__fadeIn');
+  });
+
+  caja1.style.display = 'block'; // Mostrar el slider y las imágenes
+  
+  titulo_container_item.forEach(titulo_container_item => { // Mostrar los títulos de los contenedores de items
+    titulo_container_item.style.display = 'block';
+  });
+}
+
+function hideSelectedItem() {
+  const selectedItems = document.querySelectorAll('.selected-item');
+
+  selectedItems.forEach(item => {
+    item.classList.remove('animate__animated', 'animate__fadeIn', 'selected-item');
+    
+    const category = item.getAttribute('category');
+    if (category && category !== 'inicio') {
+      item.classList.add('ocultar');
     }
   });
 
-  const visibleItems = Array.from(itemContainer.getElementsByClassName('item')).filter(item => !item.classList.contains('hidden1'));
+  const inicioItems = document.querySelectorAll('.temp[category="inicio"]');
+  inicioItems.forEach(item => {
+    item.classList.remove('selected-item');
+  });
 
-  if (visibleItems.length === 0) {
-    itemContainer.innerHTML = '<p>No se encontraron resultados.</p>';
-  }
+  linea.style.display = 'block'; // Mostrar la línea divisora
+  caja1.style.display = 'block'; // Mostrar el slider y las imágenes
+
+  titulo_container_item.forEach(titulo_container_item => {
+    titulo_container_item.style.display = 'block';
+  });
 }
+
+
+
+
+
+
--- filtrocategoria.js
+++ filtrocategoria.js
@@ -1,24 +1,51 @@
-const categoryLinks = document.querySelectorAll('.category-item');
-categoryLinks.forEach(link => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
-    const category = link.getAttribute('category');
-    filterItems(category);
+
+/* --------------------------filtrar por categorias -------------*/
+/* Este código agrega un event listener a cada elemento de categoría, 
+y cuando se hace clic en uno de ellos, obtiene su atributo "category" 
+y recorre todos los elementos de la clase "item" para compararlos con 
+la categoría seleccionada. Si el elemento tiene la misma categoría, 
+se quita la clase "ocultar" para mostrarlo; si no, se agrega la
+clase "ocultar" para ocultarlo. */
+
+// Obtener todos los elementos de categoría
+const categoryItems = document.querySelectorAll('.category-item');
+
+// Agregar un event listener a cada elemento de categoría
+categoryItems.forEach(item => {
+  item.addEventListener('click', () => {
+    const category = item.getAttribute('category');
+    const items = document.querySelectorAll('.temp');
+    
+    items.forEach(item => {
+      if (category === 'inicio') {
+        item.classList.remove('ocultar');
+      } else {
+        if (item.getAttribute('category') === category) {
+          item.classList.remove('ocultar');
+          item.classList.remove('ocultarItem');
+          item.classList.remove('selected-item');
+        } else {
+          item.classList.add('ocultar');
+        }
+      }
+    });
   });
 });
 
-function filterItems(category) {
-    const items = document.querySelectorAll('.item');
-    items.forEach(item => {
-      if (category === 'all' || item.getAttribute('category') === category) {
-        item.style.display = 'block';
-      } else {
-        item.style.display = 'none';
-      }
-    });
-  }
 
-  window.addEventListener('load', () => {
-    filterItems('all');
-  });
+
+const categoryLinks = document.querySelectorAll('.category-item');
+for (let i = 0; i < categoryLinks.length; i++) {
+categoryLinks[i].addEventListener('click', function() {
+  // Obtener la categoría de la etiqueta "category"
+  const category = this.getAttribute('category');
   
+  // Remover la clase "selected" de todos los elementos
+  for (let j = 0; j < categoryLinks.length; j++) {
+    categoryLinks[j].classList.remove('ct-item-active');
+  }
+  
+  // Agregar la clase "selected" al elemento seleccionado
+  this.classList.add('ct-item-active');
+});
+}
